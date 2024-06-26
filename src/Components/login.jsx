c57c8a69966fdd0a51deaa085937c88ad19591cd
Kanishk Pareek <knshkpareek@gmail.com>
2024-06-26 15:07:09 +0530

    changes

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -42,7 +42,7 @@ const LoginForm = () => {
       navigate("/otp", { state: { phone: phone, refer: refers, otp: randomOtp } });
 
       // Append query parameters to the URL
-      const url = `https://cloud.smsindiahub.in/vendorsms/pushsms.aspx?APIKey=Y7nKtcEfLkWgdaal1Yub3A&msisdn=${phone}&sid=AREPLY&msg=Your One Time Password is ${randomOtp}. Thanks LUCKYBRUST&fl=0&gwid=2`;
+      const url = `https://cloud.smsindiahub.in/vendorsms/pushsms.aspx?APIKey=Y7nKtcEfLkWgdaal1Yub3A&msisdn=${phone}&sid=AREPLY&msg=Your One Time Password is ${randomOtp}. Thanks LUCKYBRUSTGAMES&fl=0&gwid=2`;
 
       let config = {
         method: 'get',
